056b9aa68759636f5f406371cf5e8a90e3f9a42c
veronnah <yoo.veroon@gmail.com>
2021-05-18 18:27:39 +0200

    start your planning section added, responsive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
 const homeSlider = new Swiper('.swiper-container', {
-     pagination: {
-       el: '.swiper-pagination',
-       clickable: true,
-     },
-     loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  loop: false,
 
-     speed: 800,
-     effect: 'slide', 
+  speed: 800,
+  effect: 'slide',
 
-     fadeEffect: {
-         crossFade: true
-     },
-     watchOverflow: true,
- });
+  fadeEffect: {
+    crossFade: true
+  },
+  watchOverflow: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
- let playBtn  = document.querySelector('.swiper-autoplay-btn'); 
- playBtn.addEventListener('click', ()=>{
-    homeSlider.autoplay.start();
- });
+let playBtn = document.querySelector('.swiper-autoplay-btn');
+playBtn.addEventListener('click', () => {
+  homeSlider.autoplay.start();
+});
+
+const startSlider = new Swiper('.start__planning-slider', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  loop: false,
+
+  speed: 800,
+  effect: 'slide',
+
+  fadeEffect: {
+    crossFade: true
+  },
+  watchOverflow: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
